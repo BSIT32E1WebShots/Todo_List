15c28a4e37a7d205ce5375c8697fc3863854e5f4
Arman <Arman@ATRJOS>
2024-05-04 02:32:51 +0800

    Adding Category

--- Todolist/Models/ToDoContext.cs
+++ Todolist/Models/ToDoContext.cs
@@ -19,7 +19,7 @@ namespace Todolist.Models
                     new Category { CategoryId = "home", Name = "Home" },
                     new Category { CategoryId = "ex", Name = "Exercise" },
                     new Category { CategoryId = "shop", Name = "Shopping" },
-                    new Category { CategoryId = "call", Name = "Contact" }
+          
 
                 );
 
